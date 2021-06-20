3760ef3e1eaaca7d564a3170f85717e39a0731e4
unknown <denisdenchevneosf@gmail.com>
2021-06-20 18:38:21 +0100

    Created the front of the card with dummy data

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import styles from "./PokemonCard.module.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import image1 from '../../images/testPokemon.png'
 
-const PokemonCard = () => {
+const PokemonCard = (props) => {
+  const {image} = props;
   return (
     <>
-      <p>PokemonCard works</p>
+      <div className={styles.card}>
+        <div  className={styles.topRow}>
+          <div className={styles.imageWrapper}><img src={image1} alt="pokemon image" /></div>
+          <div className={styles.favourite}>
+            <FontAwesomeIcon icon={faHeart} className={styles.heartIcon} />
+          </div>
+        </div>
+        <div className={styles.bottomRow}>
+          <h2>Pokemon Name</h2>
+          <div >
+            <ul className={styles.listOfSkills}>
+              <li>
+                <p>One</p>
+              </li>
+              <li>
+                <p>Two</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
